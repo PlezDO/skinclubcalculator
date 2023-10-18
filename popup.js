e7c81ed1a9a6a3b8d2854cdfad4c77c8f450ebeb
PlezDO <119824546+PlezDO@users.noreply.github.com>
2023-10-18 08:55:41 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchWebsiteData()
+        .then(data => {
+            const calcualted EV = calculateEV(data);
+            document.getElementById('//TODO: left off here
+        }
+}

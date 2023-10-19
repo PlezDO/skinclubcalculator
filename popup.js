50026dc95ea0fcf43d0e8f9ca85bbffdc67540cf
Plez Ownby <plezdownby@gmail.com>
2023-10-19 00:25:05 -0400

    adapted for firefox, need to load document

--- popup.js
+++ popup.js
@@ -1,9 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchWebsiteData()
-        .then(data => {
-            const calcualted EV = calculateEV(data);
-            document.getElementById('ev').textContent = 'Expected Value:
-		$${calcualtedEV}';
-		
-        }
-}
+document.addEventListener("DOMContentLoaded", function () {
+    // Reference to the HTML elements
+    const calculateEVButton = document.getElementById("calculateEV");
+    const evResultDiv = document.getElementById("evResult");
+
+    // Event listener for the "Calculate EV" button
+    calculateEVButton.addEventListener("click", function () {
+        // Scrape the values and odds from the website (You need to implement this)
+        const values = scrapeValues(); // Implement your scraping logic
+        const odds = scrapeOdds();     // Implement your scraping logic
+
+        // Calculate the EV (You need to implement your EV calculation logic)
+        const ev = calculateEV(values, odds);
+
+        // Display the EV result in the popup
+        evResultDiv.textContent = `Expected Return (EV): $${ev.toFixed(2)}`;
+    });
+    
+    // You need to implement your scraping logic to extract values
+    function scrapeValues() {
+        // Implement your scraping logic to extract values from the website.
+        // You might use DOM manipulation to select the relevant elements and extract the data.
+        // Return the values as an array.
+        const valueElements = document.querySelectorAll('span.price');
+        const values = [];
+
+
+        value.Elements.forEach((element) => {
+            const priceText = element.textContent.trim().replace('$', '');
+            const numericValue = parseFloat(valueText);
+
+            if(!isNaN(numericValue)) {
+                values.push(numericValue);
+            }
+        });
+        
+        return values; 
+    }
+    
+    // You need to implement your scraping logic to extract odds
+    function scrapeOdds() {
+        // Implement your scraping logic to extract odds from the website.
+        // You might use DOM manipulation to select the relevant elements and extract the data.
+        // Return the odds as an array.
+    }
+
+    // You need to implement your EV calculation function here
+    function calculateEV(values, odds) {
+        // Implement your EV calculation logic based on the extracted values and odds.
+        // The 'values' and 'odds' variables should be arrays of item values and odds.
+        // You can return the calculated EV value here.
+        // Make sure to handle cases where the data is not available or invalid.
+
+        // For testing purposes, here we simply sum the values.
+        const ev = values.reduce((total, value, index) => total + (value * (odds[index] / 100)), 0);
+        return ev;
+    }
+});
+                    
